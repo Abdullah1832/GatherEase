46061e33c97a69a88f7924bcdbd90c531770e48a
Abdullah1832 <abdallahessam1832002@gmail.com>
2025-04-27 17:59:32 +0300

    use typescript instead of javascript for clerk webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,10 +1,11 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 import { clerkClient } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function POST(req) {
+export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
@@ -15,10 +16,10 @@ export async function POST(req) {
   }
 
   // Get the headers
-  const headerPayload = headers();
-  const svix_id = (await headerPayload).get("svix-id");
-  const svix_timestamp = (await headerPayload).get("svix-timestamp");
-  const svix_signature = (await headerPayload).get("svix-signature");
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -34,7 +35,7 @@ export async function POST(req) {
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt;
+  let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
@@ -42,7 +43,7 @@ export async function POST(req) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    });
+    }) as WebhookEvent;
   } catch (err) {
     console.error("Error verifying webhook:", err);
     return new Response("Error occured", {
@@ -61,7 +62,7 @@ export async function POST(req) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      username: username,
+      username: username!,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
@@ -70,7 +71,7 @@ export async function POST(req) {
     const newUser = await createUser(user);
 
     if (newUser) {
-      await clerkClient.users?.updateUserMetadata(id, {
+      (await clerkClient()).users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
@@ -86,7 +87,7 @@ export async function POST(req) {
     const user = {
       firstName: first_name,
       lastName: last_name,
-      username: username,
+      username: username!,
       photo: image_url,
     };
 
@@ -98,7 +99,7 @@ export async function POST(req) {
   if (eventType === "user.deleted") {
     const { id } = evt.data;
 
-    const deletedUser = await deleteUser(id);
+    const deletedUser = await deleteUser(id!);
 
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
